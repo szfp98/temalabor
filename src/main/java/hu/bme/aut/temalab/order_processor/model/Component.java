c48d6ae23fe9fa2b4008d84d24f79bdcf7f8522d
JUTAS\Jutas <toth.marcell.danielgmail.com>
2023-10-30 23:22:02 +0100

    Component and Product

--- src/main/java/hu/bme/aut/temalab/order_processor/model/Component.java
+++ src/main/java/hu/bme/aut/temalab/order_processor/model/Component.java
@@ -1,4 +1,15 @@
 package hu.bme.aut.temalab.order_processor.model;
 
+import java.math.BigDecimal;
+
+import hu.bme.aut.temalab.order_processor.enums.*;
+
 public class Component {
+	
+	private long id;
+	private String name;
+	private Category category;
+	private BigDecimal value;
+	private Unit unit;
+	
 }
--- src/main/java/hu/bme/aut/temalab/order_processor/model/Product.java
+++ src/main/java/hu/bme/aut/temalab/order_processor/model/Product.java
@@ -1,4 +1,14 @@
 package hu.bme.aut.temalab.order_processor.model;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import hu.bme.aut.temalab.order_processor.enums.Category;
+
 public class Product {
+	private String name;
+	private long id;
+	private Category category;
+	private BigDecimal value;
+	private List<Component> components;
 }
