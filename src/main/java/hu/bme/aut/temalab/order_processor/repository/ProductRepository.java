3540f8ea4de428d8f839b5943b8f3fa9214c2d82
andris0409 <andrisvajda71@gmail.com>
2023-11-20 11:55:36 +0100

    findbynameContaining

--- src/main/java/hu/bme/aut/temalab/order_processor/repository/ProductRepository.java
+++ src/main/java/hu/bme/aut/temalab/order_processor/repository/ProductRepository.java
@@ -1,7 +1,13 @@
 package hu.bme.aut.temalab.order_processor.repository;
 
 import hu.bme.aut.temalab.order_processor.model.Product;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByNameContaining(String name);
+
 }
