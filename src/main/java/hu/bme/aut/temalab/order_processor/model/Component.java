729ea6e491fb01b32ebf6818cf01d9064e96227d
andris0409 <andrisvajda71@gmail.com>
2023-11-20 10:07:42 +0100

    product and component class

--- src/main/java/hu/bme/aut/temalab/order_processor/model/Component.java
+++ src/main/java/hu/bme/aut/temalab/order_processor/model/Component.java
@@ -1,9 +1,10 @@
 package hu.bme.aut.temalab.order_processor.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import java.math.BigDecimal;
+
+import hu.bme.aut.temalab.order_processor.enums.Category;
+import hu.bme.aut.temalab.order_processor.enums.Unit;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -16,4 +17,14 @@ public class Component {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    private BigDecimal value;
+
+    @Enumerated(EnumType.STRING)
+    private Unit unit;
 }
--- src/main/java/hu/bme/aut/temalab/order_processor/model/Product.java
+++ src/main/java/hu/bme/aut/temalab/order_processor/model/Product.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
+import java.util.List;
+
+import hu.bme.aut.temalab.order_processor.enums.Category;
 
 @Entity
 @Getter
@@ -16,8 +19,17 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    private BigDecimal value;
+
+    private List<Component> components;
+
     public BigDecimal getPrice() {
-        //TODO
+        // TODO
         return null;
     }
 }
