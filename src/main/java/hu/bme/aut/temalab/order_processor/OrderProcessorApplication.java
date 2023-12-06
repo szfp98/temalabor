5833644c174ea65e69af9cbecc992d74d61e754f
szfp98 <pal.szabofeltothy@gmail.com>
2023-12-06 22:43:52 +0100

    Generate test data

--- src/main/java/hu/bme/aut/temalab/order_processor/OrderProcessorApplication.java
+++ src/main/java/hu/bme/aut/temalab/order_processor/OrderProcessorApplication.java
@@ -1,13 +1,25 @@
 package hu.bme.aut.temalab.order_processor;
 
+import hu.bme.aut.temalab.order_processor.service.InitDbService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RequiredArgsConstructor
 @SpringBootApplication
-public class OrderProcessorApplication {
+public class OrderProcessorApplication implements CommandLineRunner{
+
+    private final InitDbService initDbService;
 
     public static void main(String[] args) {
         SpringApplication.run(OrderProcessorApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        initDbService.initDb();
     }
 
 }
