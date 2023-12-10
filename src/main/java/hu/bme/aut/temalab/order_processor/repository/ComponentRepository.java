034cbab9ca7c0c1680dc19aa5855e2e1e253ce82
szfp98 <pal.szabofeltothy@gmail.com>
2023-12-10 16:57:21 +0100

    Delete products endpoint

--- src/main/java/hu/bme/aut/temalab/order_processor/repository/ComponentRepository.java
+++ src/main/java/hu/bme/aut/temalab/order_processor/repository/ComponentRepository.java
@@ -1,7 +1,9 @@
 package hu.bme.aut.temalab.order_processor.repository;
 
 import hu.bme.aut.temalab.order_processor.model.Component;
+import hu.bme.aut.temalab.order_processor.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ComponentRepository extends JpaRepository<Component, Long> {
+    void deleteByProduct(Product product);
 }
